f5a8304e31182887c66e7a822486c4632765c5a5
Victor <vidrums86@gmail.com>
2023-11-29 12:01:57 +0000

    Updates

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const menu = document.querySelector('.header-menu');
-
-burgerMenu.addEventListener('click',()=>{
-
-  burgerMenu.classList.toggle('active');
-  menu.classList.toggle('active');
-
-})
